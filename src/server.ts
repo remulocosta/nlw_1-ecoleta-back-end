2c51f04c22b18efe54495316b07b820d1aa41fd7
Remulo Costa <remulo.costa@gmail.com>
2020-06-10 15:41:10 -0300

    created database, migrations and seeds

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 
 import routes from './routes';
 
@@ -7,6 +8,8 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333, () => {
   console.log('🚀 Servidor rodando na porta 3333');
